45120f5c59f4e40adf8055ac55f8a790b6cca52e
Mohammad <105608394+tkiew@users.noreply.github.com>
2023-06-10 15:26:59 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from requests import get
+import asyncio
+import aiohttp
 from bs4 import BeautifulSoup
+async def getdata(q: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://www.farsroid.com/", params={"s": q}) as response:
+            req = await response.text()
+            soup = BeautifulSoup(req, "html.parser")
+            links = soup.find_all("a", {"rel": "bookmark"})
+            tasks = []
+            for link in links:
+                link, text = link["href"], link.get_text()
+                tasks.append(asyncio.create_task(getlink(link, text)))
+            results = await asyncio.gather(*tasks)
+            return results
 
-def getdata(q: str):
-    req = get("https://www.farsroid.com/", {"s": q}).text
-    soup = BeautifulSoup(req, "html.parser")
-    links = soup.find_all("a", {"rel": "bookmark"})
-    for link in links:
-        link, text = link["href"], link.get_text()
-        req = get(link).text
-        soup = BeautifulSoup(req, "html.parser")
-        download_links =  soup.find_all("a", {"class": "download-btn"})
-        yield {"link": link, "link_text": text.strip(), "download_links": [{"link": url["href"], "link_text": url.get_text().strip()} for url in download_links]}
+async def getlink(link: str, text: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(link) as response:
+            req = await response.text()
+            soup = BeautifulSoup(req, "html.parser")
+            download_links = soup.find_all("a", {"class": "download-btn"})
+            return {"link": link, "link_text": text.strip(), "download_links": [{"link": url["href"], "link_text": url.get_text().strip()} for url in download_links]}
 
 if __name__ == "__main__":
-	for info in getdata("Music"): print(info)go get github.com/PuerkitoBio/goquery
+    loop = asyncio.get_event_loop()
+    results = loop.run_until_complete(getdata("Music"))
+    print(results)
+
