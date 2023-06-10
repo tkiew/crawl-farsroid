ac2e3328e71dbc8e12b6c70c6217c1f9a34d80ac
Mohammad <105608394+tkiew@users.noreply.github.com>
2023-06-10 15:19:55 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from requests import get
 from bs4 import BeautifulSoup
 
-def get_data(q: str):
+def getdata(q: str):
     req = get("https://www.farsroid.com/", {"s": q}).text
     soup = BeautifulSoup(req, "html.parser")
     links = soup.find_all("a", {"rel": "bookmark"})
@@ -9,8 +9,8 @@ def get_data(q: str):
         link, text = link["href"], link.get_text()
         req = get(link).text
         soup = BeautifulSoup(req, "html.parser")
-        downloadable_links =  soup.find_all("a", {"class": "download-btn"})
-        yield {"link": link, "link_text": text.strip(), "download_links": [{"link": url["href"], "link_text": url.get_text().strip()} for url in downloadable_links]}
+        download_links =  soup.find_all("a", {"class": "download-btn"})
+        yield {"link": link, "link_text": text.strip(), "download_links": [{"link": url["href"], "link_text": url.get_text().strip()} for url in download_links]}
 
 if __name__ == "__main__":
-	for info in get_data("pubg"): print(info)
+	for info in getdata("Music"): print(info)go get github.com/PuerkitoBio/goquery
